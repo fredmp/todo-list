55a8d6aa05dec70f78f962e55bd24f12a1380418
Frederico Martins Porto <martinsporto@gmail.com>
2018-03-19 00:37:08 -0300

    Remove todo list

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,53 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+  $("#toggle_menu").click(function (e) {
+    $("#wrapper").toggleClass("toggled");
+    $("#toggle_menu").toggleClass("active");
+  });
+  $("ul#items>li").click(function (e) {
+    var selected = $(this).hasClass('selected');
+    $("ul#items>li").removeClass('selected');
+    if (!selected) {
+      $(this).addClass('selected');
+    }
+  });
+  $("#todo_item_content").focus(function (e) {
+    $("ul#items>li").removeClass('selected');
+  });
+  $(".add-list>a").click(function (e) {
+    e.preventDefault();
+    $(".add-list>a").hide();
+    $(".add-list>input").show();
+    $(".add-list>input").focus();
+  });
+  $(".add-list>input").keyup(function (e) {
+    if (e.keyCode == 27) {
+      $(".add-list>input").val('');
+      $(".add-list>input").hide();
+      $(".add-list>a").show();
+    }
+  });
+  $(".todo-list-item, .list-name, .remove-list").mouseenter(function (e) {
+    var element = $(e.target);
+    if (element.hasClass('todo-list-item')) {
+      $(e.target).find(".remove-list").css('display', 'inline');
+    } else if (element.hasClass('list-name')) {
+      $(e.target).next().css('display', 'inline');
+    } else if (element.hasClass('remove-list')) {
+      $(e.target).css('display', 'inline');
+    }
+  });
+  $(".todo-list-item").mouseleave(function (e) {
+    var element = $(e.target);
+    if (element.hasClass('todo-list-item')) {
+      $(e.target).find(".remove-list").css('display', 'none');
+    } else if (element.hasClass('list-name')) {
+      $(e.target).next().css('display', 'none');
+    } else if (element.hasClass('remove-list')) {
+      $(e.target).css('display', 'none');
+    }
+  });
+});
--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -5,4 +5,12 @@ class TodoListsController < ApplicationController
     todo_list.save
     redirect_to todo_list_todo_items_path(todo_list)
   end
+
+  def destroy
+    todo_list = TodoList.find(params[:id])
+    todo_list.destroy
+    first = TodoList.first
+    redirect_to first.present? ? todo_list_todo_items_path(first) : root_path
+  end
+  
 end

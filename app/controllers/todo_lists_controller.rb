d12c61f8010a7528d0f07b087ce148b10e8853ae
Frederico Martins Porto <martinsporto@gmail.com>
2018-03-18 23:34:17 -0300

    Link and input to add lists

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,8 @@
 class TodoListsController < ApplicationController
 
-  
-  
+  def create
+    todo_list = TodoList.new(params.require(:todo_list).permit(:name))
+    todo_list.save
+    redirect_to todo_list_todo_items_path(todo_list)
+  end
 end
